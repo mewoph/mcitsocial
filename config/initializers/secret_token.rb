db5b7476a59143f948be122344ca5310db434789
Mew Ophaswongse <voph@seas.upenn.edu>
2013-10-14 13:54:18 -0400

    generated new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcitsocial::Application.config.secret_token = 'c92d907d6199fd02cf062fe5da160ccbba55d067c0e9482fb51a1cef29be094963eb304b81d0325da77796531e033fa18d475b75cc7b590484e2ce9111408ba6'
