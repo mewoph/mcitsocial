d03fb576f1c6fb87c870b3e935c8bf502c603630
Connor Liddic <connorliddic@gmail.com>
2013-10-15 18:55:44 -0400

    updated landing_page

---
+++ app/assets/javascripts/landing_scripts.js
@@ -0,0 +1,15 @@
+$(function(){
+
+    var numRand = Math.floor(Math.random()*3)
+    console.log(numRand);
+
+    if (numRand == 0){
+        $('body').addClass("background-image1"); 
+    }
+    else if (numRand == 1){
+        $('body').addClass("background-image2"); 
+    }
+    else{
+        $('body').addClass("background-image3");   
+    }
+});
