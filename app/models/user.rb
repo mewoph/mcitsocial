861d23b6dae5899582aef2a1796a7c24bbad6928
Connor Liddic <connorliddic@gmail.com>
2013-10-21 16:11:25 -0400

    added .erb to view

--- app/models/user.rb
+++ app/models/user.rb
@@ -34,8 +34,78 @@
 
 class User < ActiveRecord::Base
   # attr_accessible :title, :body
-  attr_accessible :email, :password, :password_confirmation, :profile_picture
+  attr_accessible :email, :password, :password_confirmation, :profile_picture, :first_name, :last_name, :matriculation_date, :graduation_date, :bio, :undergrad_major, :undergrad_school
   has_attached_file :profile_picture, :styles => { :square => "400x400>", :thumb => "100x100>" }
   devise :database_authenticatable, :registerable, :recoverable, :confirmable, :rememberable, :trackable, :validatable, :timeoutable
 
+  #TODO Test this???
+  # Gets the full name of of the user
+  def full_name
+  	if first_name.blank? && last_name.blank?
+  		"Default Name"
+  	elsif first_name.blank?
+  		last_name
+  	elsif last_name.blank?
+  		first_name
+  	else
+  		first_name + " " + last_name
+  	end
+  end
+
+  # Returns the marticulation date in the format "Month Year", ie "September 2013"
+  def matriculation_date_in_program
+  	if !matriculation_date.blank?
+  		marticulation_date.strftime("%B %Y")
+  	else
+  		"Unknown"
+  	end
+  end
+
+  def graduation_date_in_program
+  	if !graduation_date.blank?
+  		graduation_date.strftime("%B %Y")
+  	else
+  		"Unknown"
+  	end
+  end
+
+  def full_bio
+  	if bio.blank?
+  		"No bio available."
+  	else
+  		bio
+  	end
+  end
+
+  def full_work_experience
+  	if previous_work.blank?
+  		"No work experience available."
+  	else
+  		previous_work
+  	end
+  end
+
+  def full_undergrad_school
+  	if undergrad_major.blank?
+  		"Unknown"
+  	else
+  		undergrad_major
+  	end
+  end
+
+  def full_undergrad_major
+  	if undergrad_school.blank?
+  		"Unknown"
+  	else
+  		undergrad_school
+  	end
+  end
+
+  def full_hometown
+  	if hometown.blank?
+  		"Unknown"
+  	else
+  		hometown
+  	end
+  end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,8 @@ Mcitsocial::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
   :address => "127.0.0.1",
   :port    => 25,
   :domain  => 'yourdomain.com'
