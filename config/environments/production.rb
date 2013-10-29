16a9ff24abcab4d88e43d7c57cbad14b9ebc195d
Connor Liddic <connorliddic@gmail.com>
2013-10-29 16:43:21 -0400

    more aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,15 +89,11 @@ Mcitsocial::Application.configure do
   
   config.paperclip_defaults = {
     :storage => :s3,
-    :s3_host_name => 's3-us-west-2.amazonaws.com',
-    :url => ':s3_domain_url',
-    :path => '/:class/:attachment/:id_partition/:style/:filename',
     :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
-    
   }
 
   # Log the query plan for queries taking more than this (works
