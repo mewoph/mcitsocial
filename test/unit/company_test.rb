727e274aefba1887289426d2465042ebe91a0060
Abeer Minhas <aminhas1@binghamton.edu>
2013-11-14 14:52:15 -0500

    Added unit tests for counting number of questions, counting number of comments, make company collection

--- test/unit/company_test.rb
+++ test/unit/company_test.rb
@@ -11,7 +11,19 @@
 require 'test_helper'
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "test return correct number of questions" do
+     company = Company.find(1)
+     assert_equal(3, company.num_questions)
+   end
+
+   test "test return correct number of comments" do
+     company = Company.find(1)
+     assert_equal(2, company.num_comments)
+   end
+
+   test "test return zero comments" do
+     company = Company.find(2)
+     assert_equal(0, company.num_comments)
+   end
+
 end
--- test/unit/feedback_test.rb
+++ test/unit/feedback_test.rb
@@ -1,19 +1,22 @@
 # == Schema Information
 #
-# Table name: questions
+# Table name: feedbacks
 #
-#  id         :integer          not null, primary key
-#  question   :string(255)
-#  adder_id   :integer
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  company_id :integer
+#  id               :integer          not null, primary key
+#  company_id       :integer
+#  adder_id         :integer
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  feedback_content :string(255)
+#  is_question      :boolean
 #
 
 require 'test_helper'
 
 class FeedbackTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "test make company collection method" do
+   	f = Feedback.first 
+   	expected_array = [ ['Microsoft', 1], ['Yahoo', 2]]
+   	assert_equal(f.make_company_collection, expected_array)
+   end
 end
