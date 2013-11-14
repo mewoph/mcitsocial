c48a761767be6a6cb0c0ed3eb9b42edb9591bc9d
Abeer Minhas <aminhas1@binghamton.edu>
2013-11-14 14:05:19 -0500

    Fixed more bugs in testing

--- features/step_definitions/feedback_steps.rb
+++ features/step_definitions/feedback_steps.rb
@@ -2,8 +2,6 @@
 #When
 
 When /^I view a question show page$/ do
-	puts "WE GOT HERE IN THIS PLACE OMG"
-	puts Feedback.all
 	@feedback = Feedback.first
 	visit feedback_path(@feedback)
 end
@@ -11,9 +9,10 @@ end
 #Then
 
 Then /^I should see the feedback information$/ do
+  user = User.where(:id => @feedback.adder_id)[0];
   page.should have_content @feedback.feedback_content
-  page.should have_content Users.find(@feedback.adder_id)[0].first_name
-  page.should have_content Users.find(@feedback.adder_id)[0].last_name
-  page.should have_content @feedback.created_at
+  page.should have_content user.first_name
+  page.should have_content user.last_name
+  page.should have_content @feedback.get_created_time
 end
 
