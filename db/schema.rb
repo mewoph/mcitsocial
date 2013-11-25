89c8f9911ca098ab41d5ccb60adc01ba79344722
Abeer Minhas <aminhas1@binghamton.edu>
2013-11-25 15:02:43 -0500

    Added category id to protips model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131120204714) do
+ActiveRecord::Schema.define(:version => 20131125200018) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -48,8 +48,9 @@ ActiveRecord::Schema.define(:version => 20131120204714) do
     t.integer  "adder_id"
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
   create_table "questions", :force => true do |t|
